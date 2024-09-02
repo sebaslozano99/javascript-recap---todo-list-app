994fb11d544d1e38638314c2b23a28b03ec08884
sebastian lozano <sebaslozano708@gmail.com>
2024-09-02 12:54:26 -0500

    Merge branch 'startingOver'

--- index.js
+++ index.js
@@ -1,87 +1,38 @@
-//DOM elements
+//DOM ELEMENTS --------------------------
 
 const listContainerEl = document.querySelector(".listContainer");
 const formEl = document.querySelector("form");
-const selectEl = document.querySelector("#filter");
-const inputEl = formEl.firstElementChild;
-const listContainerItems = listContainerEl.children;
+const inputEl = document.querySelector("input");
+const filterEl = document.querySelector("#filter");
 
 
-//Variables
-let tracker = []; 
+
+// State Variables -----------------------
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || []; 
 let usersInput;
+let filter = "all";
 let isUpdating = false;
 
 
 
+// Event Listeners -----------------------
 
-
-// ----------------- EVENT LISTENERS
-
-inputEl.addEventListener("keyup", (e) => {
-    usersInput = e.target.value;
-})
-
-document.addEventListener("DOMContentLoaded", backgroundImage);
+window.addEventListener("DOMContentLoaded", renderUI);
 formEl.addEventListener("submit", addOrModify);
 listContainerEl.addEventListener("click", markAsCompletedItem);
-listContainerEl.addEventListener("click", deleteItem);
+listContainerEl.addEventListener("click", deleteTask);
 listContainerEl.addEventListener("click", setAsUpdatingItem);
-selectEl.addEventListener("change", filterItems);
+filterEl.addEventListener("change", filterItems);
 
 
 
-
-
-// ----------------- Other functions
-
-//funcs that adds a background image when there are not any items yet
-function backgroundImage() {
-    if(!listContainerEl.children.length) {
-        // listContainerEl.style.backgroundImage = "url(./images/empty-box.png)";
-        listContainerEl.classList.remove("listContainer");
-        listContainerEl.classList.add("emptyList");
-    }
-
-    if(listContainerEl.children.length){
-        listContainerEl.classList.remove("emptyList");
-        listContainerEl.classList.add("listContainer");
-
-    }
-}
-
-
-function filterItems(e){
-
-    const filter = e.target.value;
-
-    for(let i = 0; i < listContainerItems.length; i++){
-        const listItem = listContainerItems[i];
-        const itemsInsideListItem = listItem.children;
-        const checkbox = itemsInsideListItem[0];
-        listItem.style.display = "flex"; //once clicked on any of the options in the select btn, we first apply this style, to make all elements visible, then depending condition below, this func will hide or reveal the selected items
-
-        if(filter === "completed" && !checkbox.checked){
-            listItem.style.display = "none";
-            
-        }
-
-        if(filter === "pending" && checkbox.checked){
-            listItem.style.display = "none";
-        }
-    }
-}
-
-
-
-
-// ----------------- Helper functions
+// Other Functions ------------------------
 
 function addOrModify(e){
     e.preventDefault();
 
-    if(!usersInput) {
-        // alert("nothing to add!");
+    if(!inputEl.value) {
         return;
     }
 
@@ -93,141 +44,167 @@ function addOrModify(e){
     }
 }
 
-function addNewListItem(){
-    const newLiEl = document.createElement("li");
-    const checkBoxEl = document.createElement("input");
-    checkBoxEl.setAttribute("type", "checkbox");
 
-    const usersTask = document.createElement("p");
-    usersTask.innerText = String(usersInput);
 
-    const buttonsContainer = document.createElement("div");
-    const deleteBtnEl = document.createElement("button");
-    deleteBtnEl.innerText = "X";
-    // deleteBtnEl.addEventListener("click", (e) => {
-    //     const targetListItem = e.target.parentElement;
-    //     targetListItem.remove();
-    // })
 
-    const updateBtlEl = document.createElement("button");
-    const iconEl = document.createElement("img");
-    iconEl.src = "./images/refresh.png";
-    iconEl.alt = "update";
-    updateBtlEl.append(iconEl);
-    updateBtlEl.classList.add("updateBtn");
 
-    buttonsContainer.append(deleteBtnEl, updateBtlEl);
 
-    newLiEl.append(checkBoxEl);
-    newLiEl.append(usersTask);
-    newLiEl.append(buttonsContainer);
+// Helper Functions -----------------------
 
-    
-    listContainerEl.append(newLiEl);
-    inputEl.value = "";
-    backgroundImage(); //After each new item submitted We call the function that checks if list is empty or not to add or remove background
-    // syncAddedItemsWithTracker(listContainerItems);
+function renderUI(){
+    if(!tasks.length) {
+        listContainerEl.classList.remove("listContainer");
+        listContainerEl.classList.add("emptyList");
+    }
+    else {
+        listContainerEl.classList.remove("emptyList");
+        listContainerEl.classList.add("listContainer");
+        let html = "";
+        tasks.forEach((task) => {
+            let segment = `
+            <li>
+                <input type="checkbox" ${task.isCompleted && "checked"}  />
+                <p class="${task.isCompleted && "completed"}" >${task.usersInput}</p>
+                <div>
+                        <button>X</button>
+                        <button class="updateBtn" ${task.isCompleted && "disabled"} style="${task.isCompleted && "background-color: #ddd;"}" >
+                            <img src="/images/refresh.png" alt="refresh" />
+                        </button>
+                </div> 
+            </li>
+            `
+            html += segment;
+            segment = "";
+        
+        })
+        listContainerEl.innerHTML = html;
+        html = ""
+    }
 
 }
 
 
-// Func -- mark as completed or unmark it -- We add eventListener to the checkboxes using event delegation
+
+function addNewListItem(){
+
+    usersInput = inputEl.value.trim();
+    tasks.push({
+        usersInput,
+        isCompleted: false,
+        isUpdating: false,
+    });
+    console.log(tasks);
+    setLocalStorage();
+    inputEl.value = "";
+    renderUI();
+}
+
+
+function setLocalStorage(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+
 
 function markAsCompletedItem(e){
     const target = e.target;
-    
-    // if(target.tagName === "BUTTON") we toggle the ".completed" css class on the paragraph element inside the list item
+
+
     if(target.matches("input")){
         const updateBtnEl = target.parentElement.children[2].children[1];
         const pEl  = target.parentElement.children[1];
         pEl.classList.toggle("completed");
 
+        tasks.forEach((element) => {
+            if(element.usersInput === pEl.innerText) {
+                element.isCompleted = !element.isCompleted;
+                setLocalStorage(tasks);
+            }
+        })
 
         if(pEl.classList.contains("completed")){
             updateBtnEl.style.backgroundColor = "#ddd";
             updateBtnEl.disabled = true;
+
         }
         else{
             updateBtnEl.style.backgroundColor = "#bbb";
             updateBtnEl.disabled = false;
         }
 
-        // syncCompletedItemsWithTracker(tracker, pEl, target);
     }
 
 
 }
 
 
-// Function to delete list item -- Event delegation to add the event listener to each of the listItem's buttons that will be generated dynamically
-
-function deleteItem(e){
+function deleteTask(e){
     const target = e.target;
 
-    if(target.tagName === "BUTTON" && !target.classList.contains("updateBtn")) {
-        target.parentElement.parentElement.remove();
-        backgroundImage();
-        // syncDeletedItemWithTracker(listContainerItems);
-        console.log(tracker);
-    }else {
-        return;
+    if(target.tagName === "BUTTON" && !target.classList.contains("updateBtn")){
+        const pEl = target.parentElement.parentElement.children[1];
+        const filteredTasks = tasks.filter(task => task.usersInput !== pEl.innerText ? task : null);
+        tasks = [...filteredTasks];
+        listContainerEl.innerHTML = "";
+        renderUI();
+        setLocalStorage();
     }
 }
 
-
-
-
-//add event listener to the updateBtn - when clicked, we toggle classList to let user know they can update the task in the item, we change the "isUpdating" variable - if "isUpdating" is set to TRUE, we pick the current text from the listItem and put it in the input and we focus it so that user knows He can modify now
 
 
 function setAsUpdatingItem(e) {
     const target = e.target;
-    
-    
-    if(target.matches(".updateBtn")){
-        document.querySelector(".addItem").innerText = "Modify";  
-        target.parentElement.parentElement.classList.toggle("updateActive");
+
+    if(target.matches(".updateBtn")) {
+        const pElValueReference = target.parentElement.parentElement.children[1].textContent;
         isUpdating = !isUpdating;
+        target.parentElement.parentElement.classList.toggle("updateActive");
 
-
-        if(isUpdating) {
+        tasks.forEach((task) => {
+            if(task.usersInput === pElValueReference) {
+                task.isUpdating = !task.isUpdating;
+            }
+        })
+        
+        if(isUpdating){
+            document.querySelector(".addItem").innerText = "Modify";  
+            const filteredItem = tasks.filter((task) => {
+                return task.usersInput === pElValueReference;
+            });
+            inputEl.value = filteredItem[0].usersInput;
             inputEl.focus();
-            inputEl.value = target.parentElement.parentElement.children[1].textContent;
-        }
-        else{
+        }else {
             inputEl.value = "";
             document.querySelector(".addItem").innerText = "Add";  
-
         }
+        
     }
+
 }
 
-
-
-// Func -- Update List Item  --- 
 
 function updateListItem(){
-    let arrayFromList = Array.from(listContainerEl.children);
-    let index;
-
-    for(let i = 0; i < arrayFromList.length; i++) {
-        if(arrayFromList[i].classList.contains("updateActive")) {
-            index = i;
+    const updatedTask = inputEl.value.trim();
+    tasks.forEach((task) => {
+        if(task.isUpdating){
+            task.usersInput = updatedTask;
+            task.isUpdating = !task.isUpdating;
         }
-    }
-
-    let pEl = arrayFromList[index].children[1];
-    pEl.innerText = usersInput;
+    })
+    setLocalStorage();
     inputEl.value = "";
+    renderUI();
     isUpdating = !isUpdating;
-    document.querySelector(".addItem").innerText = "Add";  
 
-    for(let i = 0; i < arrayFromList.length; i++){ 
-        arrayFromList[i].classList.remove("updateActive");
-    }
 }
 
 
+function filterItems(e){
+    console.log(e);
+    const target = e.target;
+    const targetValue = target.value;
+    console.log(targetValue);
 
-
-//task persistency --- localStorage
+    
+}
